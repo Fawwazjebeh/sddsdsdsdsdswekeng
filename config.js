74fd4eacd915c2b4952c54908fe36503e59c5829
Fawwazjebeh <94578909+Fawwazjebeh@users.noreply.github.com>
2022-03-06 13:45:03 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.owner = [['6281284843853', 'Fwz, true],["6281284843853"]]
+global.owner = [['6281284843853', 'Fwz', true],["6281284843853"]]
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
